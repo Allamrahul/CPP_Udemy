95f0f7dc6637c997852d21863e53efc4b66ae1be
RahulAllam <allamrahul25@gmail.com>
2021-07-29 11:55:21 +0530

    base comments regarding Inheritance

--- Inheritance/inheritance.cpp
+++ Inheritance/inheritance.cpp
@@ -16,6 +16,29 @@ using std::endl;
 
 int main() {
 
+
+    /*
+    Inheritance provides a method for creating new classes from existing classes
+    The new class contains data and behavior of existing class
+    Allows for reuse of existing class
+    Allows new classes to modify behaviors of existing classes to make it unique without actually modifying the original class
+
+    Related classes: savings account, checking account
+                    Person, Employee, student, Faculty, Staff
+
+    Ex: Account
+            balance, deposit, withdraw
+        Savings account
+            balance, deposit, withdraw, interest rate, ..
+        Checking account
+            balance, deposit, withdraw, minimum balance, per check fee
+        Trust Account
+            balance, deposit, withdraw, interest rate
+
+        We can see that these types of accounts are very different but they have some common attrubutes and behaviors
+
+        Without inheritance, if you implement the above , you would see code duplication all over
+    */
     return 0;
 
 }
