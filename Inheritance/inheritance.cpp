4cf12e9b273186818dcae248c954864986473ce2
RahulAllam <allamrahul25@gmail.com>
2021-07-29 11:32:12 +0530

    boiler plate

---
+++ Inheritance/inheritance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include <cctype>
+#include <cstring> // for c style string functions
+#include <cstdlib> // for character based functions
+#include <cmath>
+#include <stack>
+
+using std::string;
+using std::vector;
+using std::cout; 
+using std::cin; 
+using std::endl;
+
+
+int main() {
+
+    return 0;
+
+}
